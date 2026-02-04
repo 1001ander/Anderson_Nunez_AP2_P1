7baf93fd676d816296e5b2be807ac199560ea253
Anderson <@1001ander>
2026-02-04 19:50:38 -0400

    BorrameEntity

--- app/src/main/java/edu/ucne/anderson_nunez_ap2_p1/data/parcialborrame/local/BorrameEntity.kt
+++ app/src/main/java/edu/ucne/anderson_nunez_ap2_p1/data/parcialborrame/local/BorrameEntity.kt
@@ -1,4 +1,12 @@
-package edu.ucne.anderson_nunez_ap2_p1.data.parcialborrame.local
+package edu.ucne.anderson_nunez_ap2_p1.data.local
 
-class BorrameEntity {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Borrames")
+data class BorrameEntity(
+    @PrimaryKey(autoGenerate = true)
+    val borrameId: Int? = null,
+    val nombre: String = "",
+    val descripcion: String = ""
+)
