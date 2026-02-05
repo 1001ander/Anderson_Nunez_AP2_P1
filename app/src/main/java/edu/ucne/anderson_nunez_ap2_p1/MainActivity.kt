a520f5b54760c08d98bd7353d6379dcc8f9da99b
Anderson <@1001ander>
2026-02-04 20:28:59 -0400

    configurar MainActivity.

--- app/src/main/java/edu/ucne/anderson_nunez_ap2_p1/MainActivity.kt
+++ app/src/main/java/edu/ucne/anderson_nunez_ap2_p1/MainActivity.kt
@@ -5,43 +5,30 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
+import edu.ucne.anderson_nunez_ap2_p1.presentation.navigation.MainNavHost
 import edu.ucne.anderson_nunez_ap2_p1.ui.theme.Anderson_Nunez_AP2_P1Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Anderson_Nunez_AP2_P1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val navController = rememberNavController()
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    MainNavHost(
+                        navHostController = navController
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Anderson_Nunez_AP2_P1Theme {
-        Greeting("Android")
     }
 }
