ec86bcc4d9938a69facc5b6426cd51126453ffe1
Anderson <@1001ander>
2026-02-16 23:37:16 -0400

    configurar el screen

--- app/src/main/java/edu/ucne/anderson_nunez_ap2_p1/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/anderson_nunez_ap2_p1/presentation/navigation/Screen.kt
@@ -2,13 +2,10 @@ package edu.ucne.anderson_nunez_ap2_p1.presentation.navigation
 
 import kotlinx.serialization.Serializable
 
-@Serializable
 sealed class Screen {
+    @Serializable
+    data object ListMeta : Screen()
 
     @Serializable
-    data object BorrameList : Screen()
-
-    @Serializable
-    data class EditBorrame(val borrameId: Int?) : Screen()
-
+    data class EditMeta(val metaId: Int = 0) : Screen()
 }
