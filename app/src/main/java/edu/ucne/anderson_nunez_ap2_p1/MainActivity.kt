c4b09787b2dd5b354cbed927f4e25f97135dd05c
Anderson <@1001ander>
2026-02-16 23:36:12 -0400

    configurar el main activity y el navHost.

--- app/src/main/java/edu/ucne/anderson_nunez_ap2_p1/MainActivity.kt
+++ app/src/main/java/edu/ucne/anderson_nunez_ap2_p1/MainActivity.kt
@@ -4,12 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import edu.ucne.anderson_nunez_ap2_p1.presentation.navigation.MainNavHost
+import edu.ucne.anderson_nunez_ap2_p1.presentation.navigation.MetaNavHost
 import edu.ucne.anderson_nunez_ap2_p1.ui.theme.Anderson_Nunez_AP2_P1Theme
 
 @AndroidEntryPoint
@@ -19,15 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Anderson_Nunez_AP2_P1Theme {
-                val navController = rememberNavController()
-
-                Scaffold(
-                    modifier = Modifier.fillMaxSize()
-                ) { innerPadding ->
-                    MainNavHost(
-                        navHostController = navController
-                    )
-                }
+                MetaNavHost()
             }
         }
     }
--- app/src/main/java/edu/ucne/anderson_nunez_ap2_p1/presentation/navigation/MainNavHost.kt
+++ app/src/main/java/edu/ucne/anderson_nunez_ap2_p1/presentation/navigation/MainNavHost.kt
@@ -1,49 +1,39 @@
-
-
 package edu.ucne.anderson_nunez_ap2_p1.presentation.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
-//import edu.ucne.anderson_nunez_ap2_p1.presentation.borrame.edit.EditBorrameScreen
-//import edu.ucne.anderson_nunez_ap2_p1.presentation.borrame.list.ListBorrameScreen
+import edu.ucne.anderson_nunez_ap2_p1.presentation.metas.edit.EditMetaScreen
+import edu.ucne.anderson_nunez_ap2_p1.presentation.metas.list.ListMetaScreen
 
 @Composable
-fun MainNavHost(
-    navHostController: NavHostController
-) {
-    NavHost(
-        navController = navHostController,
-        startDestination = Screen.BorrameList
-    ) {
-        composable<Screen.BorrameList> {
-      /*      ListBorrameScreen(
+fun MetaNavHost() {
+    val navController = rememberNavController()
 
-                onAddClick = {
-                    navHostController.navigate(Screen.EditBorrame(borrameId = null))
+    NavHost(
+        navController = navController,
+        startDestination = Screen.ListMeta
+    ) {
+        composable<Screen.ListMeta> {
+            ListMetaScreen(
+                onAddMeta = {
+                    navController.navigate(Screen.EditMeta(metaId = 0))
                 },
-                onItemClick = { id ->
-                    navHostController.navigate(Screen.EditBorrame(borrameId = id))
+                onEditMeta = { metaId ->
+                    navController.navigate(Screen.EditMeta(metaId = metaId))
                 }
-            )*/
+            )
         }
 
-        composable<Screen.EditBorrame> { backStackEntry ->
-            val args = backStackEntry.toRoute<Screen.EditBorrame>()
-
-
-        /*    EditBorrameScreen(
-
-                onSaveClick = {
-                    navHostController.navigateUp()
-                },
-                onDeleteClick = {
-                    navHostController.navigateUp()
-                },
-                isNew = args.borrameId == null
-            )*/
+        composable<Screen.EditMeta> {
+            val args = it.toRoute<Screen.EditMeta>()
+            EditMetaScreen(
+                onNavigateBack = {
+                    navController.navigateUp()
+                }
+            )
         }
     }
 }
