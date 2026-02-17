acbdda8ad5d9055e3352e4a6caac9430de9e3863
Anderson <@1001ander>
2026-02-16 23:33:16 -0400

    crear la entidad Meta

---
+++ app/src/main/java/edu/ucne/anderson_nunez_ap2_p1/domain/metas/model/Meta.kt
@@ -0,0 +1,8 @@
+package edu.ucne.anderson_nunez_ap2_p1.domain.metas.model
+
+data class Meta(
+    val idMeta: Int = 0,
+    val descripcion: String = "",
+    val observaciones: String = "",
+    val monto: Double = 0.0
+)
